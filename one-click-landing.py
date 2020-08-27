cd0a1349f8df74f2f049d22a84f8fe5dd425dbd2
Konstantin Petrykin <kpetrykin@gmail.com>
2020-08-28 00:24:58 +0300

    Adds parsing mavlink command MAV_CMD_USER2 from QGC
    Param1 and param2 of the command - x/width and y/height of the clicked point.
    Also adds one more speed step when horizontally close to point.
    Some format editing.

--- one-click-landing.py
+++ one-click-landing.py
@@ -19,8 +19,13 @@ from clover import srv
 from std_srvs.srv import Trigger
 from mavros_msgs.srv import SetMode
 
+from pymavlink import mavutil
+from mavros import mavlink
+from mavros_msgs.msg import Mavlink
+
 # Pipeline to send video frames to QGC
-stream = cv2.VideoWriter("appsrc ! videoconvert ! video/x-raw,format=I420 ! x264enc speed-preset=ultrafast tune=zerolatency ! h264parse ! rtph264pay ! udpsink host=127.0.0.1 port=5600 sync=false", cv2.CAP_GSTREAMER, 0, 60.0, (640, 480))
+stream = cv2.VideoWriter(
+    "appsrc ! videoconvert ! video/x-raw,format=I420 ! x264enc speed-preset=ultrafast tune=zerolatency ! h264parse ! rtph264pay ! udpsink host=127.0.0.1 port=5600 sync=false", cv2.CAP_GSTREAMER, 0, 60.0, (640, 480))
 if not stream.isOpened():
     print("Stream not opened")
 
@@ -47,109 +52,143 @@ central_point_y = camera_info.K[5]
 # Focal length in pixels
 focal_length = camera_info.K[0]
 
+
+def run_tracker(x, y):
+    global tracker_initialized, cv_image, tracker
+    r = 15
+    tracker_bbox = (x - r, y - r, r * 2, r * 2)
+
+    if tracker_initialized:
+		del tracker
+		tracker = cv2.TrackerCSRT_create()
+
+	# Initialize tracker with first frame and bounding box
+    ok = tracker.init(cv_image, tracker_bbox)
+    tracker_initialized = True
+
+
 def set_tracker_bbox(event, x, y, flags, param):
-	global mouseX, mouseY, tracker_bbox, tracker_initialized, cv_image, tracker,\
-		focal_length, central_point_x, central_point_y
+    global mouseX, mouseY, tracker_bbox, tracker_initialized, cv_image, tracker,\
+        focal_length, central_point_x, central_point_y
 
-	if event == cv2.EVENT_LBUTTONDOWN:
-		
-		r = 15
-		tracker_bbox = (x - r, y - r, r * 2, r * 2)
-
-		if tracker_initialized:
-			del tracker
-			tracker = cv2.TrackerCSRT_create()
-
-		# Initialize tracker with first frame and bounding box
-		ok = tracker.init(cv_image, tracker_bbox)
-
-		print(ok)
-
-		tracker_initialized = True
+    if event == cv2.EVENT_LBUTTONDOWN:
+        run_tracker(x, y)
 
 
 # Setpoint calculation and navigation to it
 def navigate_to_calculated_setpoint(tracking_point_x, tracking_point_y, rangefinder_data):
-	real_x = ((float(tracking_point_x) - central_point_x) / focal_length) * rangefinder_data.range
-	real_y = ((float(tracking_point_y) - central_point_y) / focal_length) * rangefinder_data.range
+    real_x = ((float(tracking_point_x) - central_point_x) / \
+              focal_length) * rangefinder_data.range
+    real_y = ((float(tracking_point_y) - central_point_y) / \
+              focal_length) * rangefinder_data.range
 
-	print(math.hypot(abs(real_x), abs(real_y)))
+    print(math.hypot(abs(real_x), abs(real_y)))
 
-	# Lower speed on close horizontal distances to prevent oscillations
-	if math.hypot(abs(real_x), abs(real_y)) < 2.0:
-		speed = 0.5
-	else:
-		speed = 3.0
+    # Lower speed on close horizontal distances to prevent oscillations
+    if math.hypot(abs(real_x), abs(real_y)) < 2.0:
+        speed = 0.5
+    elif math.hypot(abs(real_x), abs(real_y)) < 5:
+        speed = 1.0
+    else:
+        speed = 3.0
 
-	print("Navigate to x: ", real_x, " y: ", real_y, " z: ", rangefinder_data.range, "speed: ", speed)
-	navigate(x=-real_y, y=-real_x, z=-rangefinder_data.range, yaw=float('nan'), speed=speed, frame_id='base_link', auto_arm=True)
+    print("Navigate to x: ", real_x, " y: ", real_y,
+          " z: ", rangefinder_data.range, "speed: ", speed)
+    navigate(x=-real_y, y=-real_x, z=-rangefinder_data.range,
+             yaw=float('nan'), speed=speed, frame_id='base_link', auto_arm=True)
 
 
 # It is for simple timer functionality
 last_time = rospy.get_time()
 
+
 def image_callback(frame):
 
-	# check to see if we have reached the end of the stream
-	if frame is None:
-		return
+    # check to see if we have reached the end of the stream
+    if frame is None:
+        return
 
-	global cv_image
-	cv_image = bridge.imgmsg_to_cv2(frame, desired_encoding='passthrough')
+    global cv_image
+    cv_image = bridge.imgmsg_to_cv2(frame, desired_encoding='passthrough')
 
-	global tracker_initialized
-	global last_time
+    global tracker_initialized
+    global last_time
 
-	global tracker
+    global tracker
 
-	if tracker_initialized:
-		(ok, bbox) = tracker.update(cv_image)
+    if tracker_initialized:
+        (ok, bbox) = tracker.update(cv_image)
 
-		# HOLD mode when lost target
-		if not ok:
-			print("Tracker reported failure")
-			set_mode(custom_mode='HOLD')
-			del tracker
-			tracker = cv2.TrackerCSRT_create()
-			tracker_initialized = false
+        # HOLD mode when lost target
+        if not ok:
+            print("Tracker reported failure")
+            set_mode(custom_mode='HOLD')
+            del tracker
+            tracker = cv2.TrackerCSRT_create()
+            tracker_initialized = False
 
-		(x, y, w, h) = [int(v) for v in bbox]
+        (x, y, w, h) = [int(v) for v in bbox]
 
-		landing_point_x = x + w/2
-		landing_point_y = y + h/2
+        landing_point_x = x + w/2
+        landing_point_y = y + h/2
 
-		cv2.circle(cv_image, (landing_point_x, landing_point_y), 1, (0, 0, 255), 1)
-		cv2.rectangle(cv_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.circle(cv_image, (landing_point_x, landing_point_y),
+                   1, (0, 0, 255), 1)
+        cv2.rectangle(cv_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-		telemetry = get_telemetry()
+        telemetry = get_telemetry()
 
-		# Let's correct our navigation setpoint using new determined coordinates
-		if rospy.get_time() - last_time > 1 and telemetry.armed:
-			rangefinder_data = rospy.wait_for_message('rangefinder/range', Range)
+        # Let's correct our navigation setpoint using new determined coordinates
+        if rospy.get_time() - last_time > 1 and telemetry.armed:
+            rangefinder_data = rospy.wait_for_message(
+                'rangefinder/range', Range)
 
-			# If we are above 2 meters
-			if rangefinder_data.range > 2.0:
-				navigate_to_calculated_setpoint(landing_point_x, landing_point_y, rangefinder_data)
-			else: # Stop tracking point, just land here
-				land()
-				if tracker:
-					tracker_initialized = False
-					del tracker
-					tracker = cv2.TrackerCSRT_create()
+            # If we are above 2 meters
+            if rangefinder_data.range > 2.0:
+                navigate_to_calculated_setpoint(
+                    landing_point_x, landing_point_y, rangefinder_data)
+            else:  # Stop tracking point, just land here
+                land()
+                if tracker:
+                    tracker_initialized = False
+                    del tracker
+                    tracker = cv2.TrackerCSRT_create()
+
+            last_time = rospy.get_time()
+
+    # show the output frame
+    cv2.imshow("Frame", cv_image)
+    cv2.setMouseCallback('Frame', set_tracker_bbox)
+
+    # Sending frame to QGC
+    stream.write(cv_image)
+
+    key = cv2.waitKey(1) & 0xFF
 
 
-			last_time = rospy.get_time()
-
-	# show the output frame
-	cv2.imshow("Frame", cv_image)
-	cv2.setMouseCallback('Frame', set_tracker_bbox)
-
-	# Sending frame to QGC
-	stream.write(cv_image)
-
-	key = cv2.waitKey(1) & 0xFF
-	
 image_sub = rospy.Subscriber('main_camera/image_raw', Image, image_callback)
+
+link = mavutil.mavlink.MAVLink('', 255, 1)
+
+param1 = 0
+param2 = 0
+
+def mavlink_message_handler(msg):
+    global param1, param2
+    if msg.msgid == 76:
+        mav_bytes_msg = mavlink.convert_to_bytes(msg)
+        mav_msg = link.decode(mav_bytes_msg)
+        if mav_msg.command == 31011:
+            print(mav_msg)
+            if mav_msg.param1 != param1 and mav_msg.param2 != param2:
+                run_tracker(640*mav_msg.param1, 480*mav_msg.param2)
+                param1 = mav_msg.param1
+                param2 = mav_msg.param2
+
+
+
+mavlink_sub = rospy.Subscriber(
+    'mavlink/from', Mavlink, mavlink_message_handler)
 
 rospy.spin()
 
